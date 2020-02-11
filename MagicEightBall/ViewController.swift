fd5ba546cc0aff9f2706ec4b04ccc0b5a20d89af
lauren <ljdutton2@icloud.com>
2020-02-11 14:48:45 -0800

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Lauren Dutton on 2/11/20.
+//  Copyright © 2020 Lauren Dutton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
